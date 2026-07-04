40debc727390fc0eddd0a4ffd4c904d92bdf2f5f
GyoriLajos <gyori_lajos88@freemail.hu>
2026-07-04 13:34:56 +0200

    Add Repositories for Entities

---
+++ src/main/java/com/example/traveling/repository/ActivityRepository.java
@@ -0,0 +1,10 @@
+package com.example.traveling.repository;
+
+import com.example.traveling.entity.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity,Long> {
+
+}
---
+++ src/main/java/com/example/traveling/repository/TravelerRepository.java
@@ -0,0 +1,10 @@
+package com.example.traveling.repository;
+
+import com.example.traveling.entity.Traveler;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TravelerRepository extends JpaRepository<Traveler,Long> {
+
+}
